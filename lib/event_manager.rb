5c3b78da3639d897ce21e45ecd3191001dbdf6ab
jhhere <joyicecloud@gmail.com>
2018-09-08 13:25:00 -0700

    Skip the header row that prints first_Name

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,7 +1,10 @@
 puts "EventManager Initialized!"
 
 lines = File.readlines "event_attendees.csv"
+row_index = 0
 lines.each do |line|
+  row_index = row_index + 1
+  next if row_index == 1
   columns = line.split(",")
   firstname = columns[2]
   puts firstname
