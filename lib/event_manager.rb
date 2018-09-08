a13637d6716ade7bbfe34ec8c8630ade4e4399ed
jhhere <joyicecloud@gmail.com>
2018-09-08 15:10:23 -0700

    what we are going to do expressed in English

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,5 +6,10 @@ contents = CSV.open "event_attendees.csv", headers: true, header_converters: :sy
 contents.each do |row|
   firstname = row[:first_name]
   zipcode = row[:zipcode]
+
+  # if the zip code is exactly five digits, assume that it is ok
+  # if the zip coe is more tan 5 digits, truncate it to the first 5 digits
+  # if the zip code is less than 5 digits, add zeros to the front until it becomes five digits
+
   puts "#{firstname} #{zipcode}"
 end
